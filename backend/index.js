4d4693728eb9e5600d5e974dc2d1073f8afaa5db
Ellu <alikoskieveliina@gmail.com>
2025-04-22 18:04:39 +0300

    dist directory to backend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 morgan.token('data', function (req, res) {return req.method === 'POST' ? JSON.stringify(req.body) : '' })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
+app.use(express.static('dist'))
+
 let phoneNumbers = [
     {
         id: "1",
